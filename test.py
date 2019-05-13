c660cc27df6fac46b8d8e20a8d25931526000e01
Philip Huang <philiphuang2@gmail.com>
2019-05-12 19:15:23 -0700

    Correct File
    Correct File

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from __future__ import print_function
 # ------------------------------------------------------------------------------------------------
 # Copyright (c) 2016 Microsoft Corporation
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 # associated documentation files (the "Software"), to deal in the Software without restriction,
 # including without limitation the rights to use, copy, modify, merge, publish, distribute,
 # sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all copies or
 # substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
 # NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -30,17 +30,18 @@ if sys.version_info[0] == 2:
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 else:
     import functools
+
     print = functools.partial(print, flush=True)
 
 # More interesting generator string: "3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"
 
-missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+missionXML = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
             <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            
+
               <About>
                 <Summary>Hello world!</Summary>
               </About>
-              
+
               <ServerSection>
                 <ServerInitialConditions>
                     <Time>
@@ -50,7 +51,7 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                     <Weather>rain</Weather>
                 </ServerInitialConditions>
                 <ServerHandlers>
-                  <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;12;"/>
+                  <FlatWorldGenerator generatorString="2;7,2x3,2;1;"/>
                   <DrawingDecorator>
                     <DrawSphere x="-27" y="70" z="0" radius="30" type="air"/>
                   </DrawingDecorator>
@@ -58,11 +59,11 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                   <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
               </ServerSection>
-              
+
               <AgentSection mode="Survival">
                 <Name>MalmoTutorialBot</Name>
                 <AgentStart>
-                    <Placement x="1" y="56" z="0" yaw="90"/>
+                    <Placement x="1" y="5" z="0" yaw="90"/>
                     <Inventory>
                         <InventoryItem slot="0" type="diamond_pickaxe"/>
                     </Inventory>
@@ -78,9 +79,9 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 
 agent_host = MalmoPython.AgentHost()
 try:
-    agent_host.parse( sys.argv )
+    agent_host.parse(sys.argv)
 except RuntimeError as e:
-    print('ERROR:',e)
+    print('ERROR:', e)
     print(agent_host.getUsage())
     exit(1)
 if agent_host.receivedArgument("help"):
@@ -94,11 +95,11 @@ my_mission_record = MalmoPython.MissionRecordSpec()
 max_retries = 3
 for retry in range(max_retries):
     try:
-        agent_host.startMission( my_mission, my_mission_record )
+        agent_host.startMission(my_mission, my_mission_record)
         break
     except RuntimeError as e:
         if retry == max_retries - 1:
-            print("Error starting mission:",e)
+            print("Error starting mission:", e)
             exit(1)
         else:
             time.sleep(2)
@@ -111,7 +112,7 @@ while not world_state.has_mission_begun:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print("Error:",error.text)
+        print("Error:", error.text)
 
 print()
 print("Mission running ", end=' ')
@@ -122,7 +123,7 @@ while world_state.is_mission_running:
     time.sleep(0.1)
     world_state = agent_host.getWorldState()
     for error in world_state.errors:
-        print("Error:",error.text)
+        print("Error:", error.text)
 
 print()
 print("Mission ended")
